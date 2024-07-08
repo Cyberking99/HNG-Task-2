aa1ab919723b1e485a3787a3420bef7aab695b7a
Kefas Kingsley <kefasiceking@gmail.com>
2024-07-08 18:54:28 +0100

    feature: added UUID for user and organisation

--- models/Organisation.js
+++ models/Organisation.js
@@ -1,11 +1,13 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
+const { v4: uuidv4 } = require('uuid');
 
 const Organisation = sequelize.define('Organisation', {
   orgId: {
     type: DataTypes.STRING,
     unique: true,
     allowNull: false,
+    defaultValue: uuidv4,
   },
   name: {
     type: DataTypes.STRING,
--- models/User.js
+++ models/User.js
@@ -1,11 +1,13 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
+const { v4: uuidv4 } = require('uuid');
 
 const User = sequelize.define('User', {
   userId: {
     type: DataTypes.STRING,
     unique: true,
     allowNull: false,
+    defaultValue: uuidv4,
   },
   firstName: {
     type: DataTypes.STRING,
